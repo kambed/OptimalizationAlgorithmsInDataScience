d3ea796c107f60fc36d8aecac497508e32985267
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2024-05-16 19:16:19 +0200

    Fix logic for bayes laplace

--- algorithms/bayes_laplace_model.py
+++ algorithms/bayes_laplace_model.py
@@ -12,7 +12,7 @@ class BayesLaplaceModel(Model):
     def solve(self, df):
         if len(df.columns) != len(self.probability):
             raise ValueError("The number of columns in the dataframe must be equal to the number of probabilities")
-        if sum(self.probability) - 1 > 1e-4:
+        if abs(sum(self.probability) - 1) > 1e-4:
             raise ValueError("The sum of probabilities must be equal to 1")
         step = (df * self.probability).sum(axis=1)
         logging.debug(step)
